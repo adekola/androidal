a2c958c306d6d440100ec671429f174eedf44c3d
adekola adebayo <adekola.m.adebayo@gmail.com>
2013-05-17 00:47:23 +0100

    inital commit for the first experiments with the android platform

---
+++ src/com/example/myfirstapp/ShowCallLogs.java
@@ -0,0 +1,20 @@
+package com.example.myfirstapp;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+public class ShowCallLogs extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+	
+}
